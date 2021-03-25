1986decf8e3422f15d68287e492de6f5c23dd5d8
rdiahern <rdiahern@everis.com>
2021-03-25 11:14:15 +0100

    Proyecto - Git - Nueva rama

--- src/com/everis/git/AppMain.java
+++ src/com/everis/git/AppMain.java
@@ -3,7 +3,7 @@ package com.everis.git;
 /**
  * Formacion - GIT
  * 
- * @author everis
+ * @author rdiahern
  *
  */
 public class AppMain {
@@ -11,17 +11,21 @@ public class AppMain {
 	/**
 	 * Metodo principal
 	 * 
-	 * @param args
+	 * @author rdiahern
 	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hola mundo");
-		
+
 		System.out.println("Hola línea evolutivo V2");
-		
+
 		System.out.println("Hola línea MASTER");
 
 		System.out.println("Hola línea evolutivo V4");
+
+		// Se añade un "Hola mundo" con el nombre del desarrollador
+
+		System.out.println("Hola Raquel");
 
 	}
 
